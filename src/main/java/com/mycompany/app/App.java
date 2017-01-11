fe40333e62bb778472d7e0e8b71a7d5e6af1b9ea
Rafael Guglielmetti <rafael@rgug.ch>
2017-01-11 11:10:11 +0100

    First commit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,73 @@
+package com.mycompany.app;
+
+import java.util.Arrays;
+
+// Basic Spark stuff
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import org.apache.spark.api.java.function.*;
+
+// For the "Word count" example
+import org.apache.spark.api.java.JavaPairRDD;
+import scala.Tuple2;
+import java.util.Iterator;
+
+/**
+* Hello world!
+*
+*/
+public class App 
+{
+	public static void main( String[] args )
+	{
+		// --------------------------------------------------------
+		// Initialization
+		System.out.println( "Hello World!" );
+
+		SparkConf conf = new SparkConf().setAppName("firstSparkProject").setMaster("local[*]");
+		JavaSparkContext sc = new JavaSparkContext(conf);
+		
+		sc.setLogLevel("WARN"); // Don't want the INFO stuff
+		
+		String path = "SPARKREADME.md";
+
+		System.out.println("Trying to open: " + path);
+		JavaRDD<String> jrdd = sc.textFile(path.toString());
+		
+		// --------------------------------------------------------
+		// First information
+		System.out.println("#lines: " + jrdd.count() );
+		System.out.println("#lines having > 5 words: " + jrdd.filter(line -> line.split(" ").length > 5 ).count());
+		System.out.println("#lines containing Spark: " + jrdd.filter(line -> line.contains("Spark")).count());
+		
+		// Number of characters in the file
+		JavaRDD<Integer> jrddLength = jrdd.map(line -> line.length());
+		System.out.println("#characters: " + jrddLength.reduce((x,y) -> x + y));
+		
+		// --------------------------------------------------------
+		// Word count
+		JavaRDD<String> words = jrdd.flatMap(new FlatMapFunction<String, String>() {
+		  public Iterator<String> call(String s) { return Arrays.asList(s.split(" ")).iterator(); }
+		});
+		System.out.println("#words: " + words.count());
+		
+		JavaPairRDD<String, Integer> pairs = words.mapToPair(new PairFunction<String, String, Integer>() {
+			public Tuple2<String, Integer> call(String s) { return new Tuple2<String, Integer>(s,1); }
+		});
+		
+		JavaPairRDD<String, Integer> wordCount = pairs.reduceByKey(new Function2<Integer, Integer, Integer>() {
+			public Integer call(Integer x, Integer y) { return x + y; }
+		});
+		
+		wordCount.saveAsTextFile(path + "_wordCount");
+		
+		// --------------------------------------------------------
+		// De-initialization
+		sc.stop();
+		sc.close();
+		
+		System.out.println("END");
+	}
+}
